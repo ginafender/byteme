1de0eb884a8d48851fddc0854c230174c0fe2659
ginafender <123966430+ginafender@users.noreply.github.com>
2024-09-19 14:00:08 -0400

    added second scene

--- byte-me/scripts/job_stand.gd
+++ byte-me/scripts/job_stand.gd
@@ -20,3 +20,7 @@ func _on_window_close_requested() -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/first_puzzle.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/puzzle2.tscn")
